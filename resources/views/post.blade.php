9aab19b16f6176f06fa44791d7b7aab75bd5c7c2
Brooke Kennison <brookelkennison@gmail.com>
2021-07-09 15:01:35 -0400

    episode 7: add individual post view

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+        <h1>First Post</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia auctor lectus lobortis feugiat. Suspendisse potenti. Quisque non laoreet ante, a condimentum elit. Praesent vitae euismod lacus. Phasellus at purus non leo volutpat dignissim vel in lectus. Quisque ornare fringilla leo, vitae varius lacus interdum vitae. Vestibulum egestas lacinia elit ac eleifend. Etiam volutpat metus eu ex elementum faucibus. Nulla a eros ligula. Phasellus sollicitudin tortor sed quam tincidunt aliquet. Nulla pretium nunc non sapien aliquet, in tristique ante ultricies. Nullam non urna id nulla malesuada scelerisque. Duis et accumsan turpis. Vestibulum vulputate mattis aliquam. Fusce ac suscipit enim.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+</html>
